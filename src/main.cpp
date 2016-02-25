97524ead2e2f350c01e8c67f62267881ecceb4c8
Matthew Williamson <chuwilliamson@gmail.com>
2016-02-25 09:36:38 -0600

    ready for students

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
-#include "SolarSystemApplication.h"
-#include "RenderingGeomApplication.h"
-#include "RenderingOBJApplication.h"
-#include "TexturingApplication.h"
-#include "TexturedOBJApplication.h"
-#include "ApplyTextureOBJApplication.h"
-#include "ProceduralGenerationApplication.h"
-#include "GUIApplication.h"
+ 
 #include "LightingOBJApplication.h"
-#include "LightingSphereApplication.h"
-#include "ParticleApplication.h"
-#include "TexturedParticleApplication.h"
+#include "LightingSphereApplication.h" 
 
 int main() {
 	
 	// change startup application
-	BaseApplication* app = new TexturedParticleApplication();
+	BaseApplication* app = new LightingSphereApplication();
 	if (app->startup())
 		app->run();
 	app->shutdown();
